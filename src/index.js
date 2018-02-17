942dedec0646cc21beb5ee1bade24879f16ce62c
Ravi Shankar <sravi4701@gmail.com>
2018-02-17 23:34:47 +0530

    add: add video_detail component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
-
+import VideoDetail from './components/video_detail';
 const API_KEY = "AIzaSyB9ES4SzaY7fyxyw5TzIXfD0Kbw73M2LcY";
 
 class App extends React.Component {
 	constructor(props){
 		super(props);
+
+		this.state = {
+			selectedVideo: null,
+			videos: []
+		}
+
+		YTSearch({key: API_KEY, term: "motivation"}, (videos) => {
+			this.setState({
+				videos: videos,
+				selectedVideo:videos[0] 
+			});
+		});
+
 	}
 
 	render() {
-		return (<SearchBar />);
+		return (
+			<div>
+				<SearchBar />
+				<VideoDetail video={this.state.selectedVideo} />
+			</div>
+		);
 	}
 }
 
