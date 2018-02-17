f448a7a36a97ef7bfc4f2f4a627ce48f6c97025c
Ravi Shankar <sravi4701@gmail.com>
2018-02-17 17:46:43 +0530

    init: initialize the app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+let App = () => {
+	return (<h1>Welcome to the Youtube ReactApp</h1>);
+}
+
+
+ReactDOM.render(<App/>, document.querySelector(".container"));
