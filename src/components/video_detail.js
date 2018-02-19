93887c4fd8d7771fe1974b98c35e81b94f208846
Ravi Shankar <sravi4701@gmail.com>
2018-02-19 23:15:27 +0530

    update: change class component to function component

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,32 +1,26 @@
 import React from 'react';
 
-class VideoDetail extends React.Component {
+
+let VideoDetail = function ({video}) {
+	if(!video){
+		return (<div>Loading........</div>);
+	}
+
+	const videoId = video.id.videoId;
+	const url = `https://www.youtube.com/embed/${videoId}`;
 	
-	constructor (props) {
-		super(props);
-	}
+	return (
+		<div className="video-detail col-md-8">	
+		<div className="embed-responsive embed-responsive-16by9">
+			<iframe className="embed-responsive-item" src={url}></iframe>
+		</div>
 
-	render () {
-		if(!this.props.video){
-			return (<div>Loading........</div>);
-		}
-
-		const videoId = this.props.video.id.videoId;
-		const url = `https://www.youtube.com/embed/${videoId}`;
-		
-		return (
-			<div className="video-detail col-md-8">	
-			<div className="embed-responsive embed-responsive-16by9">
-				<iframe className="embed-responsive-item" src={url}></iframe>
-			</div>
-
-			<div className="details">	
-				<div>{this.props.video.snippet.title}</div>
-				<div>{this.props.video.snippet.description}</div>
-			</div>
-			</div>
-		);
-	}
+		<div className="details">	
+			<div>{video.snippet.title}</div>
+			<div>{video.snippet.description}</div>
+		</div>
+		</div>
+	);
 }
 
 
