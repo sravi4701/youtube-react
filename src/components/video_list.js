66250efd6122fa5afa92e63ff1c0bd3613a49ac2
Ravi Shankar <sravi4701@gmail.com>
2018-02-19 23:06:14 +0530

    update: change classed component to functional component

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -2,23 +2,16 @@ import React from 'react';
 
 import VideoListItem from './video_list_item';
 
-class VideoList extends React.Component {
-	constructor (props) {
-		super(props);
-	}
-
-	render () {
-		let videos = this.props.videos;
-		return (
-			<ul className="col-md-4 list-group">
-				{
-					videos.map((video, index) => {
-						return <VideoListItem key={video.etag} video={video} />
-					})
-				}
-			</ul>
-		);
-	}
+let VideoList = function({videos}){
+	return (
+		<ul className="col-md-4 list-group">
+			{
+				videos.map((video, index) => {
+					return <VideoListItem key={video.etag} video={video} />
+				})
+			}
+		</ul>
+	)
 }
 
 
